9af5bee981e28aff6a848afeee19cedf33dd65ab
antipinaea <katya.19.09.01@gmail.com>
2023-03-12 14:02:19 +0700

    Initial project Add hw for lesson1

--- Lesson1/ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello, World!"); 
-    }
-}
-
---
+++ Lesson1HW/Lesson1HW/Program.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+ using System.Xml;
+
+ namespace Lesson1HW
+ {
+  public class Program
+  {
+   public static void Main(string[] args)
+   {
+    String name = Console.ReadLine();
+    
+    Console.WriteLine("Hello " + name);
+   }
+  }
+ }
